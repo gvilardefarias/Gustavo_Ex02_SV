00b08a89ef5b980b557da615e190cf8e1143a939
gvilardefarias <gvilardefarias@ymail.com>
2016-12-02 17:43:37 -0300

    Ajustes no arquivo de Teste, para usar no EDA Playground

--- 1/Teste.sv
+++ 1/Teste.sv
@@ -1,24 +1,28 @@
-module Teste;
+module Teste;  
     logic Swap, Enable, Clock;
     logic[3:0] DownCountS, UpCountS;
 
     UpDownCount Test(.Swap(Swap), .Enable(Enable), .Clock(Clock), .DownCountS(DownCountS), .UpCountS(UpCountS));
 
     initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(1);
+
         Swap = 0;
         Enable = 0;
         Clock = 0;
 
-        forever #10 Clock = ~Clock;
+        repeat(1000) #10 Clock = ~Clock;
     end
 
     initial begin
-        #20 Enable = 1;
+        #15 Enable = 1;
         #30 Enable = 0;
         #20 Enable = 1;
     end
 
-    initial forever #100 Swap = ~Swap;
-
-    $monitor(Swap, Enable, Clock, DownCountS, UpCountS);
+    initial repeat(1000) begin
+        #100 Swap = 1;
+        #15 Swap = 0;
+    end
 endmodule
