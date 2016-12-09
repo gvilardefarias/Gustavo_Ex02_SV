eaafddca63e489edc71b97f1ade45f3cc0d1426e
gvilardefarias <gvilardefarias@ymail.com>
2016-12-09 03:36:48 -0300

    Ajsutes na decima questão

--- 10/Circuito.sv
+++ 10/Circuito.sv
@@ -1,15 +1,15 @@
 module Circuito(input Clock, output logic f1, f2);
-    logic[12:0] countSubida, countDescida, saida1, saida2;
+    logic[12:0] contador;
 
-    ContadorDescida Count(.Clock(Clock), .Saida(countDescida));
+    ContadorDescida Count(.Clock(Clock), .Saida(contador));
 
     always_ff @(negedge Clock) begin
-        if(countDescida>=13'd3849 && countDescida<13'd4149)
+        if(contador>=13'd3849 && contador<13'd4149)
             f1 <= 13'd0;
         else
             f1 <= 13'd1;
 
-        if(countDescida>=13'd3199 && countDescida<13'd3799)
+        if(contador>=13'd3199 && contador<13'd3799)
             f2 <= 13'd0;
         else
             f2 <= 13'd1;
